01722ad86eff523f3a66e842ae65b7f386547761
gavincook <gavincook200@gmail.com>
2018-07-06 09:05:44 +0800

    touch gts

---
+++ gts/src/main/java/com/taobao/txc/test/Test.java
@@ -0,0 +1,14 @@
+package com.taobao.txc.test;
+
+/**
+ * Test
+ *
+ * @author gavincook
+ * @version $ID: Test.java, v0.1 2018-07-05 10:04 gavincook Exp $$
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(0x61c88647);
+    }
+}
